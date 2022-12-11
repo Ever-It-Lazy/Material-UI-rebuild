a756afcaa0d80095e2fa5415f68758da7055740f
Everett Lindsay <everettsjunk@gmail.com>
2022-12-11 18:04:45 -0500

    LandingPage loads, no Material UI adjustments just yet

--- frontend/src/screens/LandingPage/LandingPage.jsx
+++ frontend/src/screens/LandingPage/LandingPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { Container, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import "./LandingPage.css";
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const userInfo = localStorage.getItem('userInfo');
+
+		if (userInfo) {
+			navigate("/mynotes");
+		}
+	}, [navigate]);
+
+	return (
+		<div className='main'>
+			<Container>
+					<div className="intro-text">
+						<div>
+							<h1 className="title">Welcome to Note Zipper</h1>
+							<p className="subtitle">One Safe place for all your notes.</p>
+							<div className="buttonContainer">
+								<a href="/login">
+									<Button size="lg" className="landingbutton">
+										Login
+									</Button>
+								</a>
+								<a href="/register">
+									<Button
+										size="lg"
+										className="landingbutton"
+										variant="outline-primary"
+									>
+										Signup
+									</Button>
+								</a>
+							</div>
+						</div>
+					</div>
+			</Container>
+		</div>
+	)
 }
 
-export default LandingPage
+export default LandingPage;
