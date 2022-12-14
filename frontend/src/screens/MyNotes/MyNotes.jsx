e1fadfd104395ad78ceacba7462996f5e06d111b
Everett Lindsay <everettsjunk@gmail.com>
2022-12-14 18:53:29 -0500

    a temporary attempt to test the proxy server

--- frontend/src/screens/MyNotes/MyNotes.jsx
+++ frontend/src/screens/MyNotes/MyNotes.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import MainScreen from '../../components/MainScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteNoteAction, listNotes } from '../../actions/noteActions';
+import Loading from '../../components/Loading';
+import ErrorMessage from '../../components/ErrorMessage';
+import axios from 'axios';
+const MyNotes = ({ search }) => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-const MyNotes = () => {
-  return (
-    <div>MyNotes</div>
-  )
-}
+	const noteList = useSelector(state => state.noteList);
+	const { loading, notes, error } = noteList;
+	//console.log(process.env.PORT);
 
-export default MyNotes
+	const userLogin = useSelector(state => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const noteCreate = useSelector((state) => state.noteCreate);
+	const { success: successCreate } = noteCreate;
+
+	const noteUpdate = useSelector((state) => state.noteUpdate);
+	const { success: successUpdate } = noteUpdate;
+
+	const noteDelete = useSelector((state) => state.noteDelete);
+	const { loading: loadingDelete, error: errorDelete, success: successDelete } = noteDelete;
+
+	const fetchNotes = async () => {
+		const data = await axios.get("/api/notes");
+
+		console.log(data);
+	}
+
+	const deleteHandler = (id) => {
+		if (window.confirm("Are you sure?")) {
+			dispatch(deleteNoteAction(id));
+		}
+	};
+
+	useEffect(() => {
+		fetchNotes();
+	}, []);
+
+	return <MainScreen title={`Welcome Back`}>
+
+	</MainScreen>
+
+};
+
+export default MyNotes;
