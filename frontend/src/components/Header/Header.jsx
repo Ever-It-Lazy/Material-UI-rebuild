2176cf1edb797ccaf019c71ef9bf231b878edb02
Everett Lindsay <everettsjunk@gmail.com>
2022-12-15 23:41:04 -0500

    improved Search bar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
 	AppBar,
-	Box,
 	Toolbar,
 	Typography,
 	Button,
 	Menu,
 	MenuItem,
-	OutlinedInput
+	InputBase
 } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
 import { logout } from "../../actions/userActions";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	},
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+			width: '20ch',
+			},
+		},
+	},
+}));
 
 const Header = ({ setSearch }) => {
 	const [anchorEl, setAnchorEl] = useState(null);
@@ -49,14 +93,15 @@ const Header = ({ setSearch }) => {
 					</Typography>
 
 					{userInfo && (
-						<Box component="form" sx={{ flexGrow: 1 }}>
-							<OutlinedInput
-								type="text"
-								sx={{ background: 'white' }}
-								placeholder="Search"
+						<Search sx={{ }}>
+							<SearchIconWrapper>
+								<SearchIcon />
+							</SearchIconWrapper>
+							<StyledInputBase
+								placeholder="Search…"
 								onChange={(e) => setSearch(e.target.value)}
 							/>
-						</Box>
+						</Search>
 					)}
 
 					{userInfo ? (
@@ -72,6 +117,7 @@ const Header = ({ setSearch }) => {
 								sx={{ textTransform: 'none' }}
 							>
 								{userInfo.name}
+								<ArrowDropDownIcon/>
 							</Button>
 							<Menu 
 								id="menu-appbar"
