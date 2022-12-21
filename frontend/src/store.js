1a7ac87416b3ef0dd341a30e9b93a84aaeb7edfa
Everett Lindsay <everettsjunk@gmail.com>
2022-12-20 23:00:59 -0500

    round 2: removed an unused package

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,25 +2,27 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userLoginReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
 import { noteCreateReducer, noteDeleteReducer, noteListReducer, noteUpdateReducer } from './reducers/noteReducers';
 
+const reducer = {
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userUpdate: userUpdateReducer,
+	noteList: noteListReducer,
+	noteCreate: noteCreateReducer,
+	noteUpdate: noteUpdateReducer,
+	noteDelete: noteDeleteReducer
+};
+
 const userInfoFromStorage = localStorage.getItem('userInfo')
 	? JSON.parse(localStorage.getItem('userInfo'))
 	: null;
 
-const initialState = {
+const preloadedState = {
 	userLogin: { userInfo: userInfoFromStorage }
 }
 
 const store = configureStore({
-	reducer: {
-		userLogin: userLoginReducer,
-		userRegister: userRegisterReducer,
-		userUpdate: userUpdateReducer,
-		noteList: noteListReducer,
-		noteCreate: noteCreateReducer,
-		noteUpdate: noteUpdateReducer,
-		noteDelete: noteDeleteReducer
-	},
-	preloadedState: initialState
+	reducer,
+	preloadedState
 });
 
 export default store;
