5cdfd86b85bf62dfe697f818ff5e0fef1fd795ae
Everett Lindsay <everettsjunk@gmail.com>
2022-12-20 21:13:35 -0500

    round 1: upgrade to redux toolkit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,6 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { userLoginReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
 import { noteCreateReducer, noteDeleteReducer, noteListReducer, noteUpdateReducer } from './reducers/noteReducers';
-
-const reducer = combineReducers({
-	userLogin: userLoginReducer,
-	userRegister: userRegisterReducer,
-	userUpdate: userUpdateReducer,
-	noteList: noteListReducer,
-	noteCreate: noteCreateReducer,
-	noteUpdate: noteUpdateReducer,
-	noteDelete: noteDeleteReducer
-})
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
 	? JSON.parse(localStorage.getItem('userInfo'))
@@ -22,12 +10,17 @@ const initialState = {
 	userLogin: { userInfo: userInfoFromStorage }
 }
 
-const middleware = [thunk];
-
-const store = createStore(
-	reducer,
-	initialState,
-	composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+	reducer: {
+		userLogin: userLoginReducer,
+		userRegister: userRegisterReducer,
+		userUpdate: userUpdateReducer,
+		noteList: noteListReducer,
+		noteCreate: noteCreateReducer,
+		noteUpdate: noteUpdateReducer,
+		noteDelete: noteDeleteReducer
+	},
+	preloadedState: initialState
+});
 
 export default store;
