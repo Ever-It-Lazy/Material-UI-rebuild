c23fd4af2d13e49068889f56c57abe04516013d8
Everett Lindsay <everettsjunk@gmail.com>
2022-12-26 16:43:52 -0500

    set the vite build directory to /build/

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use('/api/notes', noteRoutes);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 	app.get("*", (req,res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 	});
 } else {
 	app.get('/', (req,res) => {
